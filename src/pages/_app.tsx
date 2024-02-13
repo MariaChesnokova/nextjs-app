99ad29e538a407ea881343044ec31c2145b94cb2
Maria C <mariachesnokova7@gmail.com>
2024-02-13 10:11:15 -0500

    Add common, footer, and header folders with home_page.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/common/header/Header';
+import Footer from '../components/common/footer/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
